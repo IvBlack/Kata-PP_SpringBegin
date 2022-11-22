3826c7b98b7defc15279bb78f6003736a2a8157b
IvBlack <ivbl21@ya.ru>
2022-11-22 11:09:45 +0300

    Dog.class added

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,18 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+    private String saing;
+
+
+    public Dog(String saing) {
+        this.saing = saing;
+    }
+
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
